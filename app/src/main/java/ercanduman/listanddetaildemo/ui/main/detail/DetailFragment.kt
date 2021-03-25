5cdb6bd444d9eb0f71903f5ae00f2149683379d2
Ercan Duman <ercan.duman@macellan.net>
2021-03-25 13:29:13 +0300

    Unnecessary codes removed from Fragments of ItemsFragment.kt and DetailFragment.kt

--- app/src/main/java/ercanduman/listanddetaildemo/ui/main/detail/DetailFragment.kt
+++ app/src/main/java/ercanduman/listanddetaildemo/ui/main/detail/DetailFragment.kt
@@ -1,26 +1,16 @@
 package ercanduman.listanddetaildemo.ui.main.detail
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import android.widget.Button
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import ercanduman.listanddetaildemo.R
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
  */
-class DetailFragment : Fragment() {
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_detail, container, false)
-    }
+class DetailFragment : Fragment(R.layout.fragment_detail) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
--- app/src/main/java/ercanduman/listanddetaildemo/ui/main/items/ItemsFragment.kt
+++ app/src/main/java/ercanduman/listanddetaildemo/ui/main/items/ItemsFragment.kt
@@ -1,26 +1,16 @@
 package ercanduman.listanddetaildemo.ui.main.items
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import android.widget.Button
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import ercanduman.listanddetaildemo.R
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
-class ItemsFragment : Fragment() {
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_items, container, false)
-    }
+class ItemsFragment : Fragment(R.layout.fragment_items) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
