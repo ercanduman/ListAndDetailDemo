2876f366915e682a183c86c2e401432b9c0517eb
Ercan Duman <ercan.duman@macellan.net>
2021-03-26 09:02:46 +0300

    Displaying product details in DetailFragment.kt finished

--- app/src/main/java/ercanduman/listanddetaildemo/ui/main/detail/DetailFragment.kt
+++ app/src/main/java/ercanduman/listanddetaildemo/ui/main/detail/DetailFragment.kt
@@ -3,7 +3,11 @@ package ercanduman.listanddetaildemo.ui.main.detail
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
+import com.bumptech.glide.Glide
 import ercanduman.listanddetaildemo.R
+import ercanduman.listanddetaildemo.data.model.Product
+import ercanduman.listanddetaildemo.data.network.RestApi
 import ercanduman.listanddetaildemo.databinding.FragmentDetailBinding
 
 /**
@@ -11,8 +15,26 @@ import ercanduman.listanddetaildemo.databinding.FragmentDetailBinding
  */
 class DetailFragment : Fragment(R.layout.fragment_detail) {
 
+    private val args by navArgs<DetailFragmentArgs>()
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        FragmentDetailBinding.bind(view)
+        val binding = FragmentDetailBinding.bind(view)
+
+        val product: Product = args.product
+
+        binding.apply {
+            binding.productName.text = product.name
+            binding.productDescription.text = product.description
+
+            val priceText = "${product.salePrice.currency} ${product.salePrice.amount}"
+            binding.productPrice.text = priceText
+
+            Glide.with(this@DetailFragment)
+                .load(RestApi.BASE_URL + product.url)
+                .centerCrop()
+                .error(R.drawable.ic_error_outline)
+                .into(binding.productImage)
+        }
     }
 }
