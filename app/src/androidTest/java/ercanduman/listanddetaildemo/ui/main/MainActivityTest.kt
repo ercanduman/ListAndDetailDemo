edbe2b2e88ebb65cb216e9683ee1ab9cd9880dbd
Ercan Duman <ercan.duman@macellan.net>
2021-03-26 10:07:59 +0300

    MainActivityTest.kt created to store UI test of MainActivity

---
+++ app/src/androidTest/java/ercanduman/listanddetaildemo/ui/main/MainActivityTest.kt
@@ -0,0 +1,36 @@
+package ercanduman.listanddetaildemo.ui.main
+
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import ercanduman.listanddetaildemo.R
+import org.junit.Rule
+import org.junit.Test
+
+/**
+ * Contains UI related test cases for [MainActivity].
+ *
+ * @author ercanduman
+ * @since 26.03.2021
+ */
+class MainActivityTest {
+
+    /**
+     * Creating a rule for activity scenario will run Before() methods, then the Test method, and finally any After() methods.
+     *  This way no need to use below code for all test cases:
+     *      ActivityScenario.launch(MainActivity::class.java)
+     */
+    @get:Rule
+    val activityRule = ActivityScenarioRule(MainActivity::class.java)
+
+    @Test
+    fun test_activity_launched() {
+    }
+
+    @Test
+    fun test_launch_activity_and_check_toolbar_displayed() {
+        onView(withId(R.id.toolbar)).check(matches(isDisplayed()))
+    }
+}
